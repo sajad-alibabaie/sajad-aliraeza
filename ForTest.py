f040de6e4b4e6b84c41d0cd308d79a6000623534
alirezamf80 <manafvanda@gmail.com>
2020-07-14 19:42:05 +0430

    alireza add this

--- ForTest.py
+++ ForTest.py
@@ -1,2 +1,11 @@
-print("hello world")
-print("alireza")
+radius=input("please enter radius:")
+radius=int(radius)
+pi=3.14
+masahat=pi*(radius**2)
+mohit=2*pi*radius
+diameter=2*radius
+
+
+print(masahat)
+print(mohit)
+print(diameter)
